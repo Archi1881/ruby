22e5f8398dfa90336a17352dd496881b51944dc2
Archi1881 <detr1881@gmail.com>
2020-11-16 20:50:50 +0500

    исправленный урок3

--- lesson_3/station.rb
+++ lesson_3/station.rb
@@ -7,34 +7,22 @@ class Station
     @trains = []
     @cargo = 0
     @passenger = 0
-    
   end
   
   def get_train(train)
     @trains << train
-
-    if train.type == cargo
-      @cargo += 1
-    else 
-      @passenger += 1
-    end
-    
   end
 
   def send_train(train)
     @trains.delete(train)
-    
-    if train.type == cargo
-      @cargo -= 1
-    else 
-      @passenger -= 1
-    end
-
   end
 
-  def show_list_trains
-    @trains.each { |train| puts train }
-    
+  def train_by_type(type)
+    count_trains = 0
+    @trains.each do |train|
+    if train.type == type
+    count_trains += 1
   end
 
+   puts "#{type}: #{count_trians}"
 end
--- lesson_3/train.rb
+++ lesson_3/train.rb
@@ -1,22 +1,15 @@
 class Train
-  attr_reader :count_wagon, :speed, :type, :train_route, :current_station, :current_station_index
+  attr_reader :count_wagon, :speed, :type, :train_route, :current_station, 
 
   def initialize(number, type, count_wagon,)
     @number = number
     @type = type
     @count_van = count_van
     @speed = 0
-    @train_route = nil
-    @current_station = nil
-    @current_station_index = nil
   end
 
   def go(speed) 
     @speed = speed
-  end
-
-  def current_speed  
-    @speed 
   end
 
   def stop
@@ -34,18 +27,19 @@ class Train
   end
 
   def get_route(route)
-    @current_station_index = 0
+    @route.stations.index(@current_station)
     @train_route = route
     @train_route.first_station.get_train(self)
   end
 
   def current_station
-    @train_route.stations[@current_station_index]
+    @train_route.stations[@route.stations.index(@current_station)]
   end
 
   def forward
-    if @current_station_index + 1 < @train_route.stations.length
-      @current_station_index += 1
+    
+    if @route.stations.index(@current_station)+ 1 < @train_route.stations.length
+      @route.stations.index(@current_station) += 1
       current_station.send_train(self)
       next_station.get_train(self)
     else
@@ -54,8 +48,9 @@ class Train
   end
 
   def backward
-    if @current_station_index > 0
-      @current_station_index -= 1
+    
+    if @route.stations.index(@current_station) > 0
+      @route.stations.index(@current_station) -= 1
       current_station.send_train(self)
       early_station.get_train(self)
     else
@@ -64,12 +59,10 @@ class Train
   end
 
   def next_station
-    @train_route.station[@current_station_index + 1]
+    @train_route.station[@route.stations.index(@current_station) + 1]
   end
 
   def early_station
-    @train_route.station[@current_station_index - 1]
+    @train_route.station[@route.stations.index(@current_station) - 1]
   end
 end
-
-
